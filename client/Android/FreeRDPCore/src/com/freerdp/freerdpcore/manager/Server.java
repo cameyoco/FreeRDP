1769c4ab6f14985c7139fd04ef1e3b2830dc1e8b
cameyoco <cameyo@cameyo.com>
2015-02-04 10:47:38 +0700

    Adding function Server.serverUrl

--- client/Android/FreeRDPCore/src/com/freerdp/freerdpcore/manager/Server.java
+++ client/Android/FreeRDPCore/src/com/freerdp/freerdpcore/manager/Server.java
@@ -21,13 +21,25 @@ import com.freerdp.freerdpcore.model.PackageCategoryModel;
  * This class communicate with the server.
  */
 public class Server {
-	
+
+	public static String serverUrl(String relativePage, String userEmail, String password) {
+		String httpOrhttps = Protocol.SERVER_HTTPS ? "https://" : "http://";
+		String url = httpOrhttps + Protocol.SERVER_URL + ":" + Protocol.SERVER_PORT + "/" + relativePage;
+		if (userEmail != null && !userEmail.isEmpty() && password != null && !password.isEmpty()) {
+			if (relativePage.contains("?"))
+				url += "&user=" + userEmail + "&pass=" + password;
+			else
+				url += "?user=" + userEmail + "&pass=" + password;
+		}
+		return url;
+	}
+
 	public static PackageCategoryModel[] sendLoginRequest(String userEmail, String password) {
 		String httpOrhttps = Protocol.SERVER_HTTPS ? "https://" : "http://";
 		String path = httpOrhttps + Protocol.SERVER_URL + ":" + Protocol.SERVER_PORT
 				+ "/packager.aspx?op=AccountAuth" + "&user=" + userEmail
 				+ "&pass="  + password;
-		
+
 		try {
 			URL url = new URL(path);
 			URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
@@ -38,16 +50,16 @@ public class Server {
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
-		
+
 		Log.i("login", path);
-		
+
 		String response = HttpApi.sendRequest(path, null, null);
-		
+
 		if (response == null) return null;
-		
+
 		PackageCategoryModel[] retModelArray = null;
 		try {
-			
+
 			if (response != null && !response.equals("")) {
 				JSONObject origin = new JSONObject(response);
 				JSONArray dataArray = origin.getJSONArray("libs");
@@ -57,22 +69,22 @@ public class Server {
 					for (int i = 0; i < dataArray.length(); i ++) {
 						JSONObject element = dataArray.getJSONObject(i);
 						Log.i("element", element.toString());
-						retModelArray[i] = new PackageCategoryModel(element);										
-					}					
+						retModelArray[i] = new PackageCategoryModel(element);
+					}
 				}
 			}
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		return retModelArray;		
+		return retModelArray;
 	}
-	
+
 	public static boolean sendSignUpRequest(String userEmail, String password, String subscription) {
 		String httpOrhttps = Protocol.SERVER_HTTPS ? "https://" : "http://";
 		String path = httpOrhttps + Protocol.SERVER_URL + ":" + Protocol.SERVER_PORT
 				+ "/packager.aspx?op=AccountCreate" + "&user=" + userEmail
 				+ "&pass="  + password + "&subscribe=" + subscription;
-		
+
 		try {
 			URL url = new URL(path);
 			URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
@@ -83,22 +95,22 @@ public class Server {
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
-		
+
 		Log.i("login", path);
-		
+
 		String response = HttpApi.sendRequest(path, null, null);
-		
+
 		if (response == null || !response.equals("+OK")) return false;
-		
-		return true;		
+
+		return true;
 	}
-	
+
 	public static AppModel[] getAppArray(String userEmail, String password, String lib) {
 		String httpOrhttps = Protocol.SERVER_HTTPS ? "https://" : "http://";
 		String path = httpOrhttps + Protocol.SERVER_URL + ":" + Protocol.SERVER_PORT
 				+ "/packager.aspx?op=PkgList" + "&user=" + userEmail
 				+ "&pass="  + password + "&lib=" + lib + "&detail=Player";
-		
+
 		try {
 			URL url = new URL(path);
 			URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
@@ -109,16 +121,16 @@ public class Server {
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
-		
+
 		Log.i("login", path);
-		
+
 		String response = HttpApi.sendRequest(path, null, null);
-		
+
 		if (response == null) return null;
-		
+
 		AppModel[] retModelArray = null;
 		try {
-			
+
 			if (response != null && !response.equals("")) {
 				JSONArray dataArray = new JSONArray(response);
 				Log.i("array", dataArray.toString());
@@ -136,13 +148,13 @@ public class Server {
 		}
 		return retModelArray;
 	}
-	
+
 	public static ConnectionModel sendConnectionRequest(String userEmail, String password, String packageId) {
 		String httpOrhttps = Protocol.SERVER_HTTPS ? "https://" : "http://";
 		String path = httpOrhttps + Protocol.SERVER_URL + ":" + Protocol.SERVER_PORT
 				+ "/packager.aspx?op=RdpPlay" + "&user=" + userEmail
 				+ "&pass="  + password + "&pkgId=" + packageId + "&client=Play.Android";
-		
+
 		try {
 			URL url = new URL(path);
 			URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
@@ -153,16 +165,16 @@ public class Server {
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
-		
+
 		Log.i("login", path);
-		
+
 		String response = HttpApi.sendRequest(path, null, null);
-		
+
 		if (response == null) return null;
-		
+
 		ConnectionModel retModel = null;
 		try {
-			
+
 			if (response != null && !response.equals("") && !response.startsWith("ERR:")) {
 				JSONObject origin = new JSONObject(response);
 				retModel = new ConnectionModel(origin);
@@ -172,12 +184,12 @@ public class Server {
 		}
 		return retModel;
 	}
-	
+
 	public static boolean sendRdpStatusRequest(String token) {
 		String httpOrhttps = Protocol.SERVER_HTTPS ? "https://" : "http://";
 		String path = httpOrhttps + Protocol.SERVER_URL + ":" + Protocol.SERVER_PORT
 				+ "/packager.aspx?op=RdpStatus" + "&token=" + token;
-		
+
 		try {
 			URL url = new URL(path);
 			URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
@@ -188,15 +200,15 @@ public class Server {
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
-		
+
 		Log.i("login", path);
-		
+
 		String response = HttpApi.sendRequest(path, null, null);
-		
+
 		if (response == null) return false;
-		
+
 		int retModel = Integer.valueOf(response);
-		
+
 		return retModel >= 4;
 	}
 }
