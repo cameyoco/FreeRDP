9c4d34784fd2b4d5692a633fa79ed65fc1fd8e92
cameyoco <cameyo@cameyo.com>
2015-02-04 12:10:26 +0700

    Revert "Renaming product"
    This reverts commit f12ba1315a5d98e86a69ab67b668b699da7d4cf8.

--- client/Android/aFreeRDP/AndroidManifest.xml.cmake
+++ client/Android/aFreeRDP/AndroidManifest.xml.cmake
@@ -101,7 +101,7 @@
 
         <provider
             android:name="com.freerdp.freerdpcore.services.FreeRDPSuggestionProvider"
-            android:authorities="com.cameyo.player.services.freerdpsuggestionprovider" >
+            android:authorities="com.freerdp.afreerdp.services.freerdpsuggestionprovider" >
         </provider>
     </application>
 
